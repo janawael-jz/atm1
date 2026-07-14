5d72d0201fd9bb7ba9ea99b4f9b96b23345017e2
janawael-jz <jana.ziada@gmail.com>
2026-07-14 12:10:01 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let pin = "1234"
+let balance =55
+let enterpin = prompt("enter your pin") 
+if(enterpin !==pin){
+    console.log("wrong pin")
+} else{
+    let choice =prompt("choose :1.balance 2.withdraw 3.deposite")
+} 
+let withdraw = prompt("eneter amount to withdraw")
+if(choice == "2"){ if(withdraw>balance) 
+{}
+}
