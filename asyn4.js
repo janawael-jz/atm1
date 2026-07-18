3fd7a52dceec06f180fe675f4e9ebcd3b5790bd1
janawael-jz <jana.ziada@gmail.com>
2026-07-18 19:13:30 +0300

    Create asyn4.js

---
+++ asyn4.js
@@ -0,0 +1,11 @@
+console.log("jana");
+
+setTimeout(() => {
+  console.log("jz setTimeout 1");
+}, 2000);
+
+setTimeout(() => {
+  console.log("jana setTimeout 2");
+}, 2000);
+
+console.log("jz 2");
