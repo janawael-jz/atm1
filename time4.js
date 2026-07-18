d83ccbcc281d2a0bedf6eb8181edd34c099891eb
janawael-jz <jana.ziada@gmail.com>
2026-07-18 19:19:52 +0300

    Create time4.js

---
+++ time4.js
@@ -0,0 +1,12 @@
+console.log("Program Started");
+
+setTimeout(function () {
+    console.log("Async Task");
+}, 0);
+
+let x = 10;
+let y = 20;
+
+console.log(x + y);
+
+console.log("Program Finished");
