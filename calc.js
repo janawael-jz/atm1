4b3d41a99319a63d271597a9b15d1f22d13ffb12
janawael-jz <jana.ziada@gmail.com>
2026-07-19 23:18:34 +0300

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,11 @@
+function  calculateShipping(weight){
+    const shippingCost = weight * 5;
+    
+    return new promise ((resolve ,reject)=>{
+        if(weight > 0){
+            resolve(shippingCost);
+        } else {
+            reject("Invalid weight. Please enter a positive value.");
+        }
+    })
+}
