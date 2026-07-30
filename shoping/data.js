6bde260a06f0c1a9a663efd2d351f17e3518c8a0
janawael-jz <jana.ziada@gmail.com>
2026-07-30 13:57:07 +0300

    Create data.js

---
+++ shoping/data.js
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 25000
+  },
+  {
+    id: 2,
+    name: "Mouse",
+    price: 500
+  },
+  {
+    id: 3,
+    name: "Keyboard",
+    price: 1200
+  },
+  {
+    id: 4,
+    name: "Headphones",
+    price: 1500
+  },
+  {
+    id: 5,
+    name: "Monitor",
+    price: 7000
+  }
+];
